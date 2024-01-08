15bad1b3c8a6573248264245649824b02ffa1a7a
YaroslavGrushko <grushko.kpi@gmail.com>
2024-01-08 12:39:37 +0200

    js-css-min: folder for minimized files is added

--- min-js.js
+++ min-js.js
@@ -5,10 +5,7 @@ const sourceFolder = "original/js"; // The folder where the source JS files are 
 const outputFolder = "minimized/js"; // A folder where to save minified JS files
 
 // An array of JS files to minify
-const jsFiles = [
-  "example-1.js",
-  "example-2.js",
-];
+const jsFiles = ["jquery.smooth-scroll.js", "service.js"];
 
 // Minimize JS files and save them in the specified folder
 jsFiles.forEach((file) => {
